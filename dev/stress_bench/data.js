fbf737392edbcd12ae64ec4ebb910494d84eae57
github-action-benchmark <github@users.noreply.github.com>
2024-01-04 13:33:58 +0000

    add Stress Benchmark (customSmallerIsBetter) benchmark result for 88903746ebbf3d5c4e2e33185ef5c7120b54b7cb

--- dev/stress_bench/data.js
+++ dev/stress_bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704302431644,
+  "lastUpdate": 1704375238062,
   "repoUrl": "https://github.com/Ultimaker/CuraEngine",
   "entries": {
     "Stress Benchmark": [
@@ -1251,6 +1251,42 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Ultimaker/CuraEngine/pull/2003/commits/4fbd98e2c2bd6a58d81749756796ac5818b2c1b2"
         },
         "date": 1704302430745,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Number of test cases",
+            "value": 67,
+            "unit": "-",
+            "extra": ""
+          },
+          {
+            "name": "General Stress Level",
+            "value": 47.76119402985074,
+            "unit": "%",
+            "extra": "Crashes in: 014, 038, 019, 028, 013, 008, 044, 034, 007, 016, 060, 020, 025, 045, 015, 018, 011, 059, 053, 031, 037, 039, 033, 041, 048, 002, 036, 056, 040, 029, 051, 032"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "casperlamboo@gmail.com",
+            "name": "c.lamboo",
+            "username": "casperlamboo"
+          },
+          "committer": {
+            "email": "casperlamboo@gmail.com",
+            "name": "c.lamboo",
+            "username": "casperlamboo"
+          },
+          "distinct": true,
+          "id": "88903746ebbf3d5c4e2e33185ef5c7120b54b7cb",
+          "message": "Prevent divide by zero issues",
+          "timestamp": "2024-01-04T14:27:04+01:00",
+          "tree_id": "c757820e742a3f4a3974e750b3a78d0fc1075032",
+          "url": "https://github.com/Ultimaker/CuraEngine/commit/88903746ebbf3d5c4e2e33185ef5c7120b54b7cb"
+        },
+        "date": 1704375237180,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
