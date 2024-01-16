a9c98a723ba17fa6a6fa3d08083b8ce2344f8325
github-action-benchmark <github@users.noreply.github.com>
2024-01-16 15:52:27 +0000

    add Stress Benchmark (customSmallerIsBetter) benchmark result for 518d36e2fd6fb3fba21baeb3eae77829d8153499

--- dev/stress_bench/data.js
+++ dev/stress_bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1705418996990,
+  "lastUpdate": 1705420347076,
   "repoUrl": "https://github.com/Ultimaker/CuraEngine",
   "entries": {
     "Stress Benchmark": [
@@ -2575,6 +2575,38 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Ultimaker/CuraEngine/pull/2012/commits/271a3d69fb45485ccd5e07bd75e6f1fc1e67222d"
         },
         "date": 1705418995668,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Number of test cases",
+            "value": 67,
+            "unit": "-",
+            "extra": ""
+          },
+          {
+            "name": "General Stress Level",
+            "value": 47.76119402985074,
+            "unit": "%",
+            "extra": "Crashes in: 014, 038, 019, 028, 013, 008, 044, 034, 007, 016, 060, 020, 025, 045, 015, 018, 011, 059, 053, 031, 037, 039, 033, 041, 048, 002, 036, 056, 040, 029, 051, 032"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ultimaker",
+            "username": "Ultimaker"
+          },
+          "committer": {
+            "name": "Ultimaker",
+            "username": "Ultimaker"
+          },
+          "id": "518d36e2fd6fb3fba21baeb3eae77829d8153499",
+          "message": "Fix remaining raft issues",
+          "timestamp": "2024-01-15T19:39:00Z",
+          "url": "https://github.com/Ultimaker/CuraEngine/pull/2012/commits/518d36e2fd6fb3fba21baeb3eae77829d8153499"
+        },
+        "date": 1705420345536,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
