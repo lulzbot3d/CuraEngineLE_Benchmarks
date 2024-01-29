d64e4dec302e1de91e07f004af00c8d710294da0
github-action-benchmark <github@users.noreply.github.com>
2024-01-29 17:18:04 +0000

    add Stress Benchmark (customSmallerIsBetter) benchmark result for ed2513931afbe4d284d2937189bb7a237d88e533

--- dev/stress_bench/data.js
+++ dev/stress_bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706546376937,
+  "lastUpdate": 1706548684397,
   "repoUrl": "https://github.com/Ultimaker/CuraEngine",
   "entries": {
     "Stress Benchmark": [
@@ -3723,6 +3723,38 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Ultimaker/CuraEngine/pull/2024/commits/1e267c993228b22352437ea7b6ee87a15f102a1a"
         },
         "date": 1706546375521,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Number of test cases",
+            "value": 61,
+            "unit": "-",
+            "extra": ""
+          },
+          {
+            "name": "General Stress Level",
+            "value": 52.459016393442624,
+            "unit": "%",
+            "extra": "Crashes in: 033, 020, 056, 045, 007, 060, 025, 028, 013, 002, 029, 036, 041, 015, 034, 032, 014, 051, 038, 048, 053, 037, 059, 018, 040, 039, 008, 011, 016, 031, 019, 044"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ultimaker",
+            "username": "Ultimaker"
+          },
+          "committer": {
+            "name": "Ultimaker",
+            "username": "Ultimaker"
+          },
+          "id": "ed2513931afbe4d284d2937189bb7a237d88e533",
+          "message": "Cura 11129 wall roofing",
+          "timestamp": "2024-01-26T09:50:50Z",
+          "url": "https://github.com/Ultimaker/CuraEngine/pull/2024/commits/ed2513931afbe4d284d2937189bb7a237d88e533"
+        },
+        "date": 1706548683122,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
