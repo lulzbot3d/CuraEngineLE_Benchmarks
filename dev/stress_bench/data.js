2b4bbba0def9734bd9811069073440d8a70956bb
github-action-benchmark <github@users.noreply.github.com>
2024-06-05 16:02:12 +0000

    add Stress Benchmark (customSmallerIsBetter) benchmark result for 793e742451911113764ad443e1311d46f05798d7

--- dev/stress_bench/data.js
+++ dev/stress_bench/data.js
@@ -1,40 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717593124071,
+  "lastUpdate": 1717603332412,
   "repoUrl": "https://github.com/Ultimaker/CuraEngine",
   "entries": {
     "Stress Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "name": "Ultimaker",
-            "username": "Ultimaker"
-          },
-          "committer": {
-            "name": "Ultimaker",
-            "username": "Ultimaker"
-          },
-          "id": "e7f0ac0aa0cf23c26e8109652a01bef65af2c593",
-          "message": "Fix hole expansion",
-          "timestamp": "2024-01-05T16:46:35Z",
-          "url": "https://github.com/Ultimaker/CuraEngine/pull/2003/commits/e7f0ac0aa0cf23c26e8109652a01bef65af2c593"
-        },
-        "date": 1704498558385,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Number of test cases",
-            "value": 67,
-            "unit": "-",
-            "extra": ""
-          },
-          {
-            "name": "General Stress Level",
-            "value": 47.76119402985074,
-            "unit": "%",
-            "extra": "Crashes in: 014, 038, 019, 028, 013, 008, 044, 034, 007, 016, 060, 020, 025, 045, 015, 018, 011, 059, 053, 031, 037, 039, 033, 041, 048, 002, 036, 056, 040, 029, 051, 032"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8127,6 +8095,38 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Ultimaker/CuraEngine/pull/2087/commits/8554db490bba9657ca1ddcc341e828452d18d661"
         },
         "date": 1717593122026,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Number of test cases",
+            "value": 61,
+            "unit": "-",
+            "extra": ""
+          },
+          {
+            "name": "General Stress Level",
+            "value": 0,
+            "unit": "%",
+            "extra": "Crashes in: "
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ultimaker",
+            "username": "Ultimaker"
+          },
+          "committer": {
+            "name": "Ultimaker",
+            "username": "Ultimaker"
+          },
+          "id": "793e742451911113764ad443e1311d46f05798d7",
+          "message": "NP 207 conan wasm package",
+          "timestamp": "2024-06-05T02:58:10Z",
+          "url": "https://github.com/Ultimaker/CuraEngine/pull/2084/commits/793e742451911113764ad443e1311d46f05798d7"
+        },
+        "date": 1717603330338,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
