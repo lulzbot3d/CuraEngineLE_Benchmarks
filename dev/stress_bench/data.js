016af64d2f7960a790885ea012c971b4a3dcd962
github-action-benchmark <github@users.noreply.github.com>
2024-08-05 14:30:46 +0000

    add Stress Benchmark (customSmallerIsBetter) benchmark result for b04b241489d0e01005b6de8b3872d0fe81155a9c

--- dev/stress_bench/data.js
+++ dev/stress_bench/data.js
@@ -1,40 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722859055389,
+  "lastUpdate": 1722868246622,
   "repoUrl": "https://github.com/Ultimaker/CuraEngine",
   "entries": {
     "Stress Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "name": "Ultimaker",
-            "username": "Ultimaker"
-          },
-          "committer": {
-            "name": "Ultimaker",
-            "username": "Ultimaker"
-          },
-          "id": "84bac03d5a4d99e8a869aed51f6aa90fcf74f80e",
-          "message": "Roofing walls II",
-          "timestamp": "2024-01-30T08:24:34Z",
-          "url": "https://github.com/Ultimaker/CuraEngine/pull/2026/commits/84bac03d5a4d99e8a869aed51f6aa90fcf74f80e"
-        },
-        "date": 1706633062616,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Number of test cases",
-            "value": 61,
-            "unit": "-",
-            "extra": ""
-          },
-          {
-            "name": "General Stress Level",
-            "value": 52.459016393442624,
-            "unit": "%",
-            "extra": "Crashes in: 033, 020, 056, 045, 007, 060, 025, 028, 013, 002, 029, 036, 041, 015, 034, 032, 014, 051, 038, 048, 053, 037, 059, 018, 040, 039, 008, 011, 016, 031, 019, 044"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8171,6 +8139,42 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Ultimaker/CuraEngine/commit/fac38a2b276725eae3de57f6f03a8b7b15e263b9"
         },
         "date": 1722859052775,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Number of test cases",
+            "value": 61,
+            "unit": "-",
+            "extra": ""
+          },
+          {
+            "name": "General Stress Level",
+            "value": 0,
+            "unit": "%",
+            "extra": "Crashes in: "
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alireza.doustdar@gmail.com",
+            "name": "HellAholic",
+            "username": "HellAholic"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b04b241489d0e01005b6de8b3872d0fe81155a9c",
+          "message": "Add simple full on functionality for aux fan (#2129)",
+          "timestamp": "2024-08-05T16:13:09+02:00",
+          "tree_id": "cbddbb69d76dbd944e6e16341846e97152ff7191",
+          "url": "https://github.com/Ultimaker/CuraEngine/commit/b04b241489d0e01005b6de8b3872d0fe81155a9c"
+        },
+        "date": 1722868244001,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
